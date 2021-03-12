e34c7c813d20fa1d827141b54cc19a2801d80a0e
KSFlynn007 <kelseyflynnn@gmail.com>
2021-03-12 12:06:21 -0700

    firebase connection

--- components/Chat.js
+++ components/Chat.js
@@ -5,45 +5,80 @@ import {
     KeyboardAvoidingView,
 } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class Chat extends React.Component{
     constructor() {
         super();
         this.state = {
-            messages: []
+            messages: [],
+            uid: 0,
+            user: {
+                _id: '',
+                name: '',
+                avatar: ''
+            },
+            loggedInText: ''
         }
+
+        const firebaseConfig = {
+            apiKey: "AIzaSyCY4cMbkznojqhmg94p6eFFqHmFbUP-2f8",
+            authDomain: "chat-app-11762.firebaseapp.com",
+            projectId: "chat-app-11762",
+            storageBucket: "chat-app-11762.appspot.com",
+            messagingSenderId: "1094671831669",
+            appId: "1:1094671831669:web:f1f6cacb335dbf23eb1e3c",
+            measurementId: "G-MLDD6PXQRB"
+
+        }
+
+        if(!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
+
+        this.referenceChatMessages = firebase.firestore().collection('messages');
+        firebase.firestore().collection('messages').doc('messages');
     }
-    componentDidMount() {
+
+    onCollectionUpdate = (querySnapshot) => {
+        const messages = [];
+        querySnapshot.forEach((doc) => {
+            let data = doc.data();
+            messages.push({
+                _id: data._id,
+                text: data.text,
+                createdAt: data.createdAt.toDate(),
+                user: {
+                    _id: data.user._id,
+                    name: data.user.name,
+                    avatar: data.user.avatar,
+                }
+            });
+        });
         this.setState({
-            messages: [
-                // messages require id, creation data and user object, ptional items are image, video, etc.
-                // reference "Message Object" section of GiftedChat repo 
-                {
-                    _id: 1,
-                    text: 'Hello developer!',
-                    createdAt: new Date(),
-                    user: {
-                        _id: 2,
-                        name: 'React Native',
-                        avatar: 'https://placeimg.com/140/140/any',
-                    },
-                },
-                // grey-scaled message above all others, system message used for something like "A has entered chat!", etc.
-                {
-                    _id: 2,
-                    text: `Hi ${this.props.route.params.name}, welcome to the chat!`,
-                    createdAt: new Date(),
-                    system: true
-                },
-            ]
+            messages,
         })
+    }
+
+    addMessage() {
+        const message = this.state.messages[0];
+        this.referenceChatMessages.add({
+            _id: message._id,
+            createdAt: message.createdAt,
+            text: message.text || null,
+            user: message.user,
+        });
     }
 
     onSend(messages = []) {
         this.setState(previousState => ({
             // .append is built into GiftedChat component
             messages: GiftedChat.append(previousState.messages, messages),
-        }))
+        }),
+        () => {
+            this.addMessage();
+        });
     }
 
     renderBubble(props) {
@@ -65,11 +100,54 @@ export default class Chat extends React.Component{
             />
         );
     }
+
+    componentDidMount() {
+        // calls firebase auth to app.
+        this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+            if (!user) {
+                await firebase.auth().signInAnonymously();
+            }
+            // update user with current active user data
+            this.setState({
+                user: {
+                    _id: user.uid,
+                    name: this.props.route.params.name,
+                    avatar: 'https://placeimg.com/140/140/any'
+                },
+                messages: [],
+                loggedInText: 'Hello there'
+            });
+            this.referenceChatMessages = firebase.firestore().collection('messages');
+            this.unsubscribe = this.referenceChatMessages
+                .orderBy('createdAt', 'desc')
+                .onSnapshot(this.onCollectionUpdate);
+        });
+        this.systemWelcomeMessage();
+    }
+
+    systemWelcomeMessage() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: `Hi ${this.props.route.params.name}, welcome to the chat!`,
+                    createdAt: new Date(),
+                // grey-scaled message above all others, system message used for something like "A has entered chat!", etc.
+                    system: true
+                },
+            ]
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+        this.authUnsubscribe();
+    }
     
 
     render() {
         // pulling props from Start.js as passed in onPress
-        let {name, color} = this.props.route.params
+        const {name, color} = this.props.route.params;
 
         // props user's name into title of chat message
         this.props.navigation.setOptions({ title: name })
@@ -80,9 +158,7 @@ export default class Chat extends React.Component{
                 renderBubble={this.renderBubble.bind(this)}
                 messages={this.state.messages}
                 onSend={messages => this.onSend(messages)}
-                user={{
-                    _id: 1
-                }}
+                user={this.state.user}
                 />
                 {/* old android keyboard covering fix */}
                 {Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height'/> : null}
